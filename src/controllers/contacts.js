b17b4b37db10a4d2e23a9224217f8004d8e153ee
Lyuzika <lyuzika@gmail.com>
2024-10-09 20:01:49 +0300

    files add

---
+++ src/controllers/contacts.js
@@ -0,0 +1,31 @@
+import { getAllContacts, getContactsById } from '../services/contacts.js';
+import createHttpError from 'http-errors';
+
+export const getContactsController = async (req, res, next) => {
+  try {
+    const contacts = await getAllContacts();
+
+    res.json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getContactByIdController = async (req, res) => {
+  const { contactId } = req.params;
+  const contact = await getContactsById(contactId);
+
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
--- src/server.js
+++ src/server.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-// import { env } from './utils/env.js';
-import dotenv from 'dotenv';
-import { getAllContacts, getContactsById } from './services/contacts.js';
+import { env } from './utils/env.js';
+// import dotenv from 'dotenv';
+import studentsRouter from './router/students.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
-dotenv.config();
-const PORT = Number(process.env.PORT);
+// dotenv.config();
 
-export function setupServer() {
+// const PORT = Number(process.env.PORT);
+const PORT = Number(env('PORT', '3000'));
+
+export const setupServer = () => {
   const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
   app.use(
     pino({
       transport: {
@@ -17,7 +25,6 @@ export function setupServer() {
       },
     }),
   );
-  app.use(cors());
 
   app.get('/', (req, res) => {
     res.json({
@@ -25,46 +32,13 @@ export function setupServer() {
     });
   });
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
+  app.use(studentsRouter);
 
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await getContactsById(contactId);
+  app.use('*', notFoundHandler);
 
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
-      });
-      return;
-    }
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
-
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-}
+};
