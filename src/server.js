e77b187735dcac7536d2ca348004033c07de0d6b
Lyuzika <lyuzika@gmail.com>
2024-10-10 19:16:03 +0300

    some upd

--- src/server.js
+++ src/server.js
@@ -2,14 +2,11 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-// import dotenv from 'dotenv';
+
 import studentsRouter from './router/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
-// dotenv.config();
-
-// const PORT = Number(process.env.PORT);
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
